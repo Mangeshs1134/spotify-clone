0203d89da8af5a9956075e3b507a110fd0debe20
Mangesh Gupta <63158834+Mangeshs1134@users.noreply.github.com>
2024-10-24 11:42:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ async function getSongs(folder) {
   
   let songName = [];
   let artistName = [];
-  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
